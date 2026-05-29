b0924581c555097abe588116c1ed64d695534d19
Aidan <aidanlee1980@gmail.com>
2026-05-30 00:41:27 +0800

    Fixed display error 2

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ st.title("AI study assistant")
 uploaded_file = st.file_uploader("Upload your notes", type = ["txt"])
 if uploaded_file:
     content = uploaded_file.read().decode()
-    st.subheader("Your Notes")
-    st.text_area(content, height = 300)
+    #st.subheader("Your Notes")
+    st.text_area("Your Notes", content, height = 300)
 
 question = st.text_input("Ask a question")
 if question:
