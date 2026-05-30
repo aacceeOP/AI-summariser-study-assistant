05da3c7431d06ec21997a52ba1c6b77d9ba5faec
Aidan <aidanlee1980@gmail.com>
2026-05-31 00:23:18 +0800

    Connected gemini api for note based question answering

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from utils import ask_gemini
+
 st.title("AI study assistant")
 uploaded_file = st.file_uploader("Upload your notes", type = ["txt"])
 if uploaded_file:
@@ -6,7 +8,8 @@ if uploaded_file:
     #st.subheader("Your Notes")
     st.text_area("Your Notes", content, height = 300)
 
-question = st.text_input("Ask a question")
-if question:
-    st.write("You asked: {temp_1}".format(temp_1 = question))
-    st.write("Answer: Im not too sure!")
+    question = st.text_input("Ask a question")
+    if question:
+        st.write("You asked: {temp_1}".format(temp_1 = question))
+        answer = ask_gemini(content, question)
+        st.write("Answer: {answers}".format(answers = answer))
