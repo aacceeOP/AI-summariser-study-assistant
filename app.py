826a69ac3ae22b2fcd174906e1aec7f1b6ce9b4e
Aidan <aidanlee1980@gmail.com>
2026-05-31 00:12:43 +0800

    Added gemini integration

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import streamlit as st
 st.title("AI study assistant")
 uploaded_file = st.file_uploader("Upload your notes", type = ["txt"])
 if uploaded_file:
-    content = uploaded_file.read().decode()
+    content = uploaded_file.read().decode()#("utf8")
     #st.subheader("Your Notes")
     st.text_area("Your Notes", content, height = 300)
 
 question = st.text_input("Ask a question")
 if question:
     st.write("You asked: {temp_1}".format(temp_1 = question))
+    st.write("Answer: Im not too sure!")
---
+++ utils.py
@@ -0,0 +1,23 @@
+import os 
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def ask_gemini(notes, question):
+    client = genai.Client(api_key = os.getenv("GEMINI_API_KEY"))
+
+    prompt = f"""
+You are a helpful study assitant.
+
+Use only the notes below to answer the question 
+
+Notes:
+{notes}
+
+Question:
+{question}
+"""
+    response = client.models.generate_content(model = "gemini-2.5-flash", contents = prompt)
+
+    return response.text
